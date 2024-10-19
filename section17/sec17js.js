4fa1e11b2bb133cb117435b288bc767c1b9a4842
Namanverma991 <nnamanvverma@gmail.com>
2024-10-20 00:52:40 +0530

    section17

---
+++ section17/sec17js.js
@@ -0,0 +1,92 @@
+//array
+
+// //push 
+// let arr = [1,2,3,4];
+// arr.push(5);
+// console.log(arr);
+
+// //pop
+// arr.pop();
+// console.log(arr);
+
+// //shift
+// arr.shift();
+// console.log(arr);
+
+// //unshift
+// arr.unshift(0);
+// console.log(arr);
+
+// //concatination in two array
+// let arr1 = [1,2,3,4];
+// let arr2 = [5,6,7,8];
+// let arr = arr1.concat(arr2);
+// console.log(arr);
+
+// //index
+// let name = ["abc","xyz","mno","pqr"];
+// console.log(name.indexOf("xyz"));
+// let a = name.includes("psm");
+// console.log(a);
+// let b = "abc".indexOf("c");
+// console.log(b);
+// console.log(name);
+// let c = name.reverse();
+// console.log(c);
+
+// //slice
+// let name = ["abc","xyz","mno","pqr"];
+// let a = name.slice(2,5);
+// console.log(a);
+
+// //splice
+// let name = ["abc","xyz","mno","pqr"];
+// let a = name.splice(3,1);
+// console.log(a);
+// console.log(name);
+// let b = name.splice(3,0,"qwe")
+// console.log(b);
+// console.log(name);
+
+// //sorting
+// let num = [3,4,6,8,9,1,7,0];
+// num.sort();
+// console.log(num);
+
+// //equality testing
+// let a = 1 === 1;
+// console.log(a);
+
+// let b = [1,2,3] === [1,2,3];
+// console.log(b);
+
+// num = [4,5,6,7];
+// let numcopy = num;
+// console.log(numcopy);
+// let c = num.push(8);
+// console.log(c);
+// console.log(num);
+// console.log(numcopy);
+// let d = num === numcopy;
+// console.log(d);
+
+// //gameboard
+// let gameset = [["x", "0", "x"], [null, "0", "x"], ["x", "0", "x"]];
+// console.log(gameset);
+// let a = gameset[1][2];
+// console.log(a);
+
+// //practice
+// // DO NOT TOUCH!!! (please)
+// const airplaneSeats = [
+//     ['Ruth', 'Anthony', 'Stevie'],
+//     ['Amelia', 'Pedro', 'Maya'],
+//     ['Xavier', 'Ananya', 'Luis'],
+//     ['Luke', null, 'Deniz'],
+//     ['Rin', 'Sakura', 'Francisco']
+// ];
+
+// // YOUR CODE GOES BELOW THIS LINE:
+
+// airplaneSeats[3][1] = "Hugo";
+// console.log(airplaneSeats);
