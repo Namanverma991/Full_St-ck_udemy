4e0f32d1b0ef717f26a2e1da472fd79de2697eb4
Namanverma991 <nnamanvverma@gmail.com>
2024-10-20 00:49:03 +0530

    added section16

---
+++ section16/js1.js
@@ -0,0 +1,124 @@
+// console.log("hii meri jann kesi hai");
+
+// let gand = 6;
+// if (gand === 6){
+//     console.log("gandhi ji ka bosdha");
+// }
+
+// const rand = prompt("enter rate:");
+// let rate = Math.floor(Math.random()* 2);
+// if (rate === rand){
+//     console.log("rand is good");
+//     console.log(rate);
+// }
+// console.log(rand);
+// console.log(rate);
+
+// function isEven(num){
+//     //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+//    if(num / 2 === 0){
+//        console.log("even");
+//    }
+   
+//     //AND THIS LINE ↑↑↑↑↑
+// }
+
+// let day = "saturday";
+// if(day === "monday"){
+//     console.log("i hate monday");
+// }
+// else if(day === "saturday"){
+//     console.log("i love saturday");
+// }
+
+// const age = prompt("enter a age");
+// if(age == 0){
+//     console.error("that not possible")
+//     console.log("error");
+// }
+// else if(age <= 5){
+//     console.log("kid");
+// }
+// else if(age <= 10){
+//     console.log("child");
+// }
+// else if(age <= 18){
+//     console.log("teenager");
+// }
+// else if(age <= 60){
+//     console.log("adult");
+// }
+// else{
+//     console.log("senior");
+// }
+
+// let password = prompt("enter a new password");
+// if(password.length < 9){
+//     if(password.indexOf(" ") === -1){
+//         console.log("password is invalid");
+//     }
+//     else{
+//         console.log("password is valid");
+//     }
+//     console.log("password is short");
+// }
+// else{
+//     console.log("password is good");
+// }
+// password check
+// Change the value of num, so that "YOU GOT ME!" prints out
+// const num = prompt("enter number");// THIS IS THE ONLY LINE YOU SHOULD CHANGE :) 
+
+// DO NOT TOUCH ANYTHING BELOW (please) 
+// if(num < 100) {
+//     if(num / 1 === 0) {
+//         console.log("HEY!");
+//     }
+// }
+// else {
+//     if (num <= 103) {
+//         if(num % 2 === 0){
+//             console.log("YOU GOT ME!");
+//         }
+//     }
+// }  
+
+// const password = prompt("enter a strong password");
+// if(password.length >= 6 && password.indexOf(" ") === -1){
+//     console.log("password is valid");
+// }
+// else{
+//     console.log("incorrect format")
+// }
+
+
+// const mystery = 'Pretty 7lease'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+// LEAVE THIS CODE ALONE! (pretty please)
+// if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+//     console.log("YOU GOT IT!!!");
+// }
+
+// const day = 3;
+// switch (day){
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("thursday");
+//         break;
+//     case 5:
+//         console.log("friday");
+//         break;
+//     case 6:
+//         console.log("saturday");
+//         break;
+//     case 7:
+//         console.log("sunday");
+//         break;
+// }
