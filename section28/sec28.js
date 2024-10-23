771791a79f81f15dac80fafe93e51c4703212bb0
Namanverma991 <nnamanvverma@gmail.com>
2024-10-23 21:45:58 +0530

    sec

---
+++ section28/sec28.js
@@ -0,0 +1,14 @@
+// const req = new XMLHttpRequest();
+
+// req.onload = function(){
+//     console.log("its working");
+//     console.log(this);
+// }
+
+// req.onerror = function(){
+//     console.log("it error")
+//     console.log(this)
+// }
+
+// req.open("GET", "https://swapi.dev/api/planets/1/");
+// req.send();
