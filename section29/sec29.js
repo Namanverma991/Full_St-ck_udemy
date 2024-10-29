7a40fe6cbc7cb51f339b032919f5731c83b8d075
Namanverma991 <nnamanvverma@gmail.com>
2024-10-29 21:52:51 +0530

    class

--- section29/sec29.js
+++ section29/sec29.js
@@ -6,30 +6,42 @@
 //     return "i forget to fucked up sorry"
 // }
 
-function hex(r,g,b){
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+// function hex(r,g,b){
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
 
-function rgb(r,g,b){
-    retirn `${r}, ${g}, ${b}`;
-}
+// function rgb(r,g,b){
+//     retirn `${r}, ${g}, ${b}`;
+// }
 
-function makecolor(r,g,b){
-    const color = {};
-    color.r = r;
-    color.g = g;
-    color.b = b;
-    color.rgb = function(){
-        const {r,g,b} = this;
-        return `rgb(${r}, ${g}, ${b})`;
+// function makecolor(r,g,b){
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function(){
+//         const {r,g,b} = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+//     }
+//     color.hex = function(){
+//         const {r,g,b} = this;
+//         return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//     }
+//     return color;
+// }
+
+// const firstcolor = makecolor(35,255,150);
+// firstcolor.hex();
+
+//class constructor
+class color{
+    constructor(r,g,b,name){
+        console.log("inside the color");
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
     }
-    color.hex = function(){
-        const {r,g,b} = this;
-        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-    }
-    return color;
 }
 
-const firstcolor = makecolor(35,255,150);
-firstcolor.hex();
-
+const c1 = new color(255, 67, 78, "fuck")
