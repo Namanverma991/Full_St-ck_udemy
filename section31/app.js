e99c504c9c6603a86ec60c3b5559171a9071bb68
Namanverma991 <nnamanvverma@gmail.com>
2024-10-30 23:54:16 +0530

    basic

---
+++ section31/app.js
@@ -0,0 +1,3 @@
+const math = require("./sec31");
+console.log(math.square(9));
+
---
+++ section31/sec31.js
@@ -0,0 +1,6 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const square = (x) => x*x;
+
+module.exports.add = add;
+module.exports.square = square;
