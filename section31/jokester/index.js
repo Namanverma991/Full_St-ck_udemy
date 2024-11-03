adc0b13564cefa0e5417eeef7222d9c1c730c807
Namanverma991 <nnamanvverma@gmail.com>
2024-11-03 23:55:46 +0530

    color

--- section31/jokester/index.js
+++ section31/jokester/index.js
@@ -1,6 +1,7 @@
 const jokes = require('give-me-a-joke');
+const color = require("colors");
 console.dir(jokes);
 
 jokes.getRandomDadJoke (function(joke) {
-     console.log(joke);
+     console.log(joke.rainbow);
 });
