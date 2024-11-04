0ff5c908db90e9ab0d7936e9d93db38c03a88b9b
Namanverma991 <nnamanvverma@gmail.com>
2024-11-04 23:01:55 +0530

    langnotworking

---
+++ section31/language/index.js
@@ -0,0 +1,7 @@
+var franc = require("franc");
+const langs = require("langs");
+
+var v1 = franc('Alle menslike wesens word vry') 
+var v2 = franc('এটি একটি ভাষা একক IBM স্ক্রিপ্ট') 
+var v3 = franc('Alle menneske er fødde til fridom')
+
