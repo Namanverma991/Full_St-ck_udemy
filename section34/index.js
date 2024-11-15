2ae12f180c2b617903c999d6d3674a7f2dcbbbe3
Namanverma991 <nnamanvverma@gmail.com>
2024-11-15 23:34:55 +0530

    ejs

---
+++ section34/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req, res) => {
+    res.render("home.ejs");
+    
+})
+
+app.listen(3000, () => {
+    console.log("bye");
+})
+
