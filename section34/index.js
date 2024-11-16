a00d41a2bd59ed3387cfa802547eadba586f9a42
Namanverma991 <nnamanvverma@gmail.com>
2024-11-16 20:40:05 +0530

    thats it

--- section34/index.js
+++ section34/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("views engine", "ejs");
+app.set("views", path.join(__dirname,"/views"));
 
 app.get("/",(req, res) => {
     res.render("home.ejs");
     
 })
 
-app.listen(3000, () => {
+app.get("/.cats",(req,res) => {
+    const cats = [
+        "max", "wax", "tax", "hax"
+    ]
+    res.render("cats.ejs", { cats });
+})
+
+app.get("/.rand",(req,res) =>{
+    const num = Math.floor(Math.random() * 100) + 1;
+    res.render("random.ejs", { num });
+})
+
+
+app.listen(3001, () => {
     console.log("bye");
 })
 
